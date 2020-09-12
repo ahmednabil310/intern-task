9be6c6b332f1d0006cea8c83e8d4e08930886cc6
Ahmed <an30458@gmail.com>
2020-09-13 01:49:49 +0200

    starting

--- src/component/DataList.js
+++ src/component/DataList.js
@@ -1,31 +1,8 @@
 import React, { Component } from "react";
 import data from "../data/data.json";
-import "antd/dist/antd.css";
-import { List } from "antd";
-import "./datalist.css";
 export default class DataList extends Component {
   render() {
     const listData = data;
-    return (
-      <div className="list-container">
-        <List
-          itemLayout="vertical"
-          size="large"
-          pagination={{
-            pageSize: 8,
-          }}
-          dataSource={listData}
-          renderItem={(listData) => (
-            <List.Item key={listData.title} actions={[]}>
-              <List.Item.Meta
-                title={<a href={listData.name}>{listData.name}</a>}
-                description={listData.age}
-              />
-              {listData.country}
-            </List.Item>
-          )}
-        />
-      </div>
-    );
+    return <div></div>;
   }
 }
